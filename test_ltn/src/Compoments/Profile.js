9cbe7c3a64358e6e5e2cc3a068f9ba95bb87c3f0
yassine elati <yassine.elati@epitech.eu>
2019-10-11 12:56:07 +0200

    add infos in profile

--- test_ltn/src/Compoments/Profile.js
+++ test_ltn/src/Compoments/Profile.js
@@ -10,6 +10,11 @@ export default class Profile extends React.Component {
                     <img className={"imgDetail"} alt={"img"} src={"https://res.cloudinary.com/dcyafbpoh/image/upload/c_fill,f_auto,g_face,h_200,q_auto,w_250/kg4hu0apoumxozfjw7mh"}/>
                     <div className={"infos"} >
                         <h3 className={"name"}>Dyslexic Swing & The Silent Brocoli</h3>
+                        <p>Jazz / Guinguette<br/>
+                            5/5<br/>
+                            à partir de 790 €
+                        </p>
+                        <button type="button" className="btn btn-outline-light">Reserver</button>
                     </div>
                 </div>
             </div>
