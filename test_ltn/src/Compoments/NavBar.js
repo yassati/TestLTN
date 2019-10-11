786d1cc655c8dea47ae371dd42bc31a8f1b27cde
yassine elati <yassine.elati@epitech.eu>
2019-10-11 11:16:07 +0200

    navBar

---
+++ test_ltn/src/Compoments/NavBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Button} from 'reactstrap';
+
+export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">LIVETONIGHT</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">DEVIS</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <Button color="btn btn-dark">Connexion // Inscription</Button>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
